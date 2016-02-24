907591b4309fed739d27c1a750e0bc2642c23996
JWSuitUp <wangjia@dickinson.edu>
2016-02-24 14:41:58 -0500

    got readString working. Need to fix the buf index problem

--- project2/kernel.c
+++ project2/kernel.c
@@ -6,25 +6,34 @@ void putChar(char letter, int row, int col, int color);
 void putString(char* letter, int row, int col, int color);
 int printString(char *str);
 int readChar();
-
+int readString(char *buf);
 //main method
 int main(){
     
-
-    
-    char * buf = "*****";
+    char * buf = "***********************************";
     char ch;
     int fullCh;
     char scanCode;
-    printString("Hello Joanna\0");
-    printString("Type a char: \0");
-    ch = readChar();
-    buf[2] = ch;
-    printString("Read: \0");
-    printString(buf);
-    printString("\n\r\0");
+    char * line = "                        ";
+    char a;
     
-    // just read the character.
+    //Test printString
+    //printString("Hello Joanna\0");
+    
+    //Test readChar
+    //printString("Type a char: \0");
+    //ch = readChar();
+    //buf[2] = ch;
+    //printString("Read: \0");
+    //printString(buf);
+    //printString("\n\r\0");
+
+    //Test readString
+    printString("Enter a line: \0");
+    readString(buf);
+    printString("\n\0");
+    printString(buf);
+    
 
 }
 
@@ -40,13 +49,13 @@ int printString(char *str){
     }
      result = i - 1;
     
-    for(;result >= 0;result--){
-        char al = '*';
-        char ah = 0x0E;
-        int ax = ah * 256 + al;
-        interrupt(0x10, ax, 0, 0, 0);
-        
-    }
+//    for(;result >= 0;result--){
+//        char al = '*';
+//        char ah = 0x0E;
+//        int ax = ah * 256 + al;
+//        interrupt(0x10, ax, 0, 0, 0);
+//        
+//    }
     return result;
     
 }
@@ -55,24 +64,35 @@ int readChar(){
     return interrupt(0x16, 0x00, 0, 0, 0);
 }
 
-
+int readString(char *buf){
+    int i = 0;
+    int result;
+    while(buf[i] != '\0'&& (i == 0 ||buf[i-1] != 0x0D)){
+        result =readChar()%256;
+        buf[i] =result;
+        i++;
+    }
+    
+    buf[i++] = 0;
+    return i - 1;
+}
 
 //the putChat function
 //Parameters: letter to be printed, row and col of the desired location, color of the letters
-//void putChar(char letter, int row, int col, int color){
-//    int letterAddress = (80*(row -1) + col)*2 + 0x8000; // calculate the address to store the character
-//    putInMemory(0xB000, letterAddress, letter); //calculate the address to store the color
-//    putInMemory(0xB000, letterAddress + 1, color);
-//}
+void putChar(char letter, int row, int col, int color){
+    int letterAddress = (80*(row -1) + col)*2 + 0x8000; // calculate the address to store the character
+    putInMemory(0xB000, letterAddress, letter); //calculate the address to store the color
+    putInMemory(0xB000, letterAddress + 1, color);
+}
 
 //the putString function
 //Parameters: string to be printed, row and col of the desired location(begining of the string), color of the string
-//void putString(char* letter, int row, int col, int color){
-//    int i = 0;
-//    int add = (80*(row -1) + col);
-//    while(letter[i] != '\0' ){
-//        putInMemory(0xB000, (add + i)*2 + 0x8000, letter[i]); //calculate the address to store the character
-//        putInMemory(0xB000, (add + i)*2 + 0x8000 + 1, color); //calculate the address to store the color
-//        i++;
-//    }
-//}
+void putString(char* letter, int row, int col, int color){
+    int i = 0;
+    int add = (80*(row -1) + col);
+    while(letter[i] != '\0' ){
+        putInMemory(0xB000, (add + i)*2 + 0x8000, letter[i]); //calculate the address to store the character
+        putInMemory(0xB000, (add + i)*2 + 0x8000 + 1, color); //calculate the address to store the color
+        i++;
+    }
+}
---
+++ project2/testOS.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ../PathSetter.bash
+bcc -ansi -c -o kernel.o kernel.c #compile the kernel
+as86 kernel.asm -o kernel_asm.o #assemble the kernel.asm file
+ld86 -o kernel -d kernel.o kernel_asm.o #link the kernel.o and kernel_asm.o files into the executable kernel file
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3 #copy the kernel file to sector 3 use
+#dd if=kernel of=message.txt bs=512 conv=notrunc seek=30 #place the provided message.txt file into sector 30 of the disk
+
